52606c8cb1a0d4952a251c51c37ba93961fa63c3
RepikM <moriel4@outlook.com>
2018-11-06 12:28:31 -0800

    js loops

--- 11.6.2018/main.js
+++ 11.6.2018/main.js
@@ -1,1 +1,16 @@
 alert("something");
+var x = document.getElementById("a").innerHTML;	
+var i= 0;
+for(i=0; i <4; i++){
+	document.getElementsByTagName("p")[i].innerHTML= "kukariky";
+}
+var j = 0 ;
+for(j=0; j <2; j++){
+document.getElementsByClassName("lel")[j].innerHTML= "kukarikyLELEL";	
+
+}
+var k;
+for(k=0; k <2; k++){
+document.getElementsByClassName("kek")[k].innerHTML= "kukarikyKEKEKEEK";	
+
+}
